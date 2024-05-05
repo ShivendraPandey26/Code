7a14a66b12ededf105d91bd115893d94a301b3ee
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-05-05 12:47:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import { configDotenv } from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 configDotenv({
-    path: './env'
-})
+  path: "./env",
+});
 
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PROT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PROT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+  });
